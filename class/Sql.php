c0af695543e0442eb054130f16ef230d118a27ad
tiago <tiagofeli.rk@gmail.com>
2021-02-22 12:16:06 -0300

    inicio

---
+++ class/Sql.php
@@ -0,0 +1,53 @@
+<?php
+
+class Sql extends PDO {
+
+    private $conn;
+
+    public function __construct()
+    {
+
+        $this->conn = new PDO("mysql:dbname=dpphp7;host=localhost", "root", "");
+
+    }
+
+    private function setParams($statment, $parameters = array())
+    {
+        foreach ($parameters as $key => $value)
+        {
+
+            $this->setParam($key, $value);
+
+        }
+
+    }
+
+    private function setParam($statment, $key,$value)
+    {
+
+        $statment->bindParam($key,$value);
+
+    }
+
+    public function query($rawQuery, $params = array())
+    {
+        $stmt = $this->conn->prepare($rawQuery); //prepara a linha 
+
+        $this->setParams($stmt,$params);
+
+        $stmt->execute();
+
+        return $stmt;
+
+    }
+
+    public function select($rawQuery, $params = array())
+    {
+
+        $stmt = $this->query($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO ::FETCH_ASSOC); //retorna um array simples 
+
+    }
+
+}
---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+/*    spl_autoload_register(
+        function ($class_name)
+        {
+            $filename= $class_name.'.php';
+
+            if(file_exists(($class_name)))
+            {
+                require_once ($filename);
+            }
+        }
+    );*/
+
+function incluirClasse($classeNome)
+{
+    require_once ('class' . DIRECTORY_SEPARATOR . $classeNome.".php");
+
+}
+
+
+spl_autoload_register("incluirClasse");
+
